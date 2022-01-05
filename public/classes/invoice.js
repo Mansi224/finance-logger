07d38efb5fd0538a5d6382258eaade0f4515211c
mansi <ladkarmansi@gmail.com>
2022-01-05 13:08:12 +0530

    second commit

--- public/classes/invoice.js
+++ public/classes/invoice.js
@@ -1,1 +1,10 @@
-"use strict";
+export class Invoice {
+  constructor(client, details, amount) {
+    this.client = client;
+    this.details = details;
+    this.amount = amount;
+  }
+  format() {
+    return `${this.client} owes £${this.amount} for ${this.details}`;
+  }
+}
